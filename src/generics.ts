03fe86c2f87d876bbd209d038b8c5f1cb3ab3e40
Matthew Fisher <mwfisher91@gmail.com>
2022-03-13 15:02:21 -0700

    update

---
+++ src/generics.ts
@@ -0,0 +1,55 @@
+function simpleState<T>(initial: T): [() => T, (v: T) => void] {
+  let val: T = initial;
+  return [
+    () => val,
+    (v: T) => {
+      val = v;
+    },
+  ];
+}
+
+const [st1Getter, st1Setter] = simpleState(10);
+
+console.log(st1Getter());
+st1Setter(62);
+console.log(st1Getter());
+
+const [st2Getter, st2Setter] = simpleState<string | null>(null);
+console.log(st2Getter());
+st2Setter("Yo");
+console.log(st2Getter());
+
+interface Rank<T> {
+  item: T;
+  rank: number;
+}
+
+function ranker<RankItem>(items: RankItem[], rankTransformer: (v: RankItem) => number): RankItem[] {
+  const ranks: Rank<RankItem>[] = items.map((item) => ({
+    item,
+    rank: rankTransformer(item),
+  }));
+
+  ranks.sort((a, b) => a.rank - b.rank);
+
+  return ranks.map((rank) => rank.item);
+}
+
+interface Pokemon {
+  name: string;
+  hp: number;
+}
+
+const pokemon: Pokemon[] = [
+  {
+    name: "Bulbasaur",
+    hp: 20,
+  },
+  {
+    name: "Ivysaur",
+    hp: 5,
+  },
+];
+
+const ranks = ranker(pokemon, ({ hp }) => hp);
+console.log(ranks);
